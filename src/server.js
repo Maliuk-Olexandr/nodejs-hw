b9a96e42586373826b920284ad08a5365bd7623b
Maliuk-Olexandr <maamcd100@gmail.com>
2025-09-27 21:03:05 +0300

    add cors and pino for logging; implement note retrieval routes and error handling middleware

--- src/server.js
+++ src/server.js
@@ -1,11 +1,57 @@
-
 import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
 const app = express();
 const PORT = process.env.PORT || 3030;
+app.use(cors());
+app.use(express.json());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
 
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:id', (req, res) => {
+  const { id } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${id}` });
+});
+
+// Маршрут для тестування middleware помилки
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+// Middleware 404 (після всіх маршрутів)
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// Middleware для обробки помилок
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({
+    message: err.message,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`);
