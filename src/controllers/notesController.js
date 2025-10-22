e5dfb6a8ddd352f88ba834753c7ca40a42c23d0b
Oleksandr Maliuk <maliukolexandr@gmail.com>
2025-10-22 09:09:59 +0300

    feat: enhance note retrieval with pagination and filtering by tag and search text

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,8 +2,42 @@ import createHttpError from 'http-errors';
 import Note from '../models/note.js';
 
 export const getAllNotes = async (req, res) => {
-  const notes = await Note.find();
-  res.status(200).json(notes);
+  const page = Number(req.query.page) || 1;
+  const perPage = Number(req.query.perPage) || 10;
+  const { tag, search } = req.query;
+  const skip = (page - 1) * perPage;
+
+
+  const filter = {};
+
+  if (tag) {
+    filter.tags = tag;
+  }
+  if (search && search.trim() !== '') {
+    filter.$text = { $search: search };
+  }
+
+  // if (search && search.trim() !== '') {
+  //   filter.$or = [
+  //     { title: { $regex: search, $options: 'i' } },
+  //     { content: { $regex: search, $options: 'i' } },
+  //   ];
+  // }
+
+  const [notes, totalItems] = await Promise.all([
+    Note.find(filter).skip(skip).limit(perPage).exec(),
+    Note.countDocuments(filter),
+  ]);
+
+  const totalPages = Math.ceil(totalItems / perPage);
+
+  res.status(200).json({
+    notes,
+    page,
+    perPage,
+    totalItems,
+    totalPages,
+  });
 };
 
 export const getNoteById = async (req, res, next) => {
@@ -33,7 +67,9 @@ export const deleteNote = async (req, res, next) => {
 
 export const updateNote = async (req, res, next) => {
   const { noteId } = req.params;
-  const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, { new: true });
+  const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, {
+    new: true,
+  });
   if (!note) {
     next(createHttpError(404, 'Note not found'));
     return;
